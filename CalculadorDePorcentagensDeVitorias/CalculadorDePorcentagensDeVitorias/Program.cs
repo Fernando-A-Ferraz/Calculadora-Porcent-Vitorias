c2912077deb182d3416f9f207d93e1c8460a4e27
Fernando Augusto Ferraz <100580357+Fernando-A-Ferraz@users.noreply.github.com>
2024-09-13 08:08:45 -0300

    finalizado

---
+++ CalculadorDePorcentagensDeVitorias/CalculadorDePorcentagensDeVitorias/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorcentJogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao calculador de porcentagem!");
+            Console.WriteLine( "Insira o numero de jogos: ");
+            int games = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o numero de vitórias:");
+            int wins = Convert.ToInt32(Console.ReadLine());
+
+            //creating the player object
+            Player player1 = new Player();
+            player1.games = games;
+            player1.wins = wins;
+
+            //output
+            player1.GetWinRate();
+        }
+    }
+
+    class Player
+    {
+        public int games;
+        public int wins;
+        //winrate is private
+        private int winrate;
+
+        //complete the method
+        public void GetWinRate()
+        {
+            winrate = (int)((double)wins / games * 100);
+            Console.WriteLine("Numero em porcentagens:");
+            Console.WriteLine($" {winrate} %");
+        }
+    }
+}
